407c736278768f53409283467bb952ad18b25a56
tech-whiz <mbachibuikevictor@gmail.com>
2022-09-16 16:09:54 +0100

    Modify index.js

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function nameTap(tapper) {
     console.log(tapper);
 }
 nameTap("This is the value of the function parameter");
+
+
+let secureKey = "Encrypted_Key"
+function callKey(pundit) {
+    console.log(`${pundit} ${secureKey}`);
+    return pundit;
+}
+
+callKey("This is the classfied key:");
+let stdout = document.getElementsByClassName("asap")[0].innerHTML= callKey("This is the classfied key:") ` ${secureKey}`;
